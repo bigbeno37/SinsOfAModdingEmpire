83993d0fcfc6bf076a82b050b5aabe1deb57bc59
Ben O'Sullivan <bigbeno37@gmail.com>
2017-10-29 21:33:50 +1100

    Added new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,17 @@ import { HomeComponent } from './components/home/home.component';
 import { AppRoutingModule } from './app-routing.module';
 
 import { ElectronService } from './providers/electron.service';
+import { ModsComponent } from './components/mods/mods.component';
+import { ModDetailComponent } from './components/mod-detail/mod-detail.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ModsComponent,
+    ModDetailComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
