81089f07a88249f94fde43287c375491c5799261
Ben O'Sullivan <bigbeno37@gmail.com>
2017-10-30 15:28:39 +1100

    Added mod folder

---
+++ src/app/components/mod/mod.component.ts
@@ -0,0 +1,23 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Mod} from "../../models/Mod";
+
+@Component({
+  selector: 'app-mod',
+  templateUrl: './mod.component.html',
+  styleUrls: ['./mod.component.scss']
+})
+export class ModComponent implements OnInit {
+
+  @Input() mod: Mod;
+  @Output() modClicked = new EventEmitter<Mod>();
+
+  onModClick() {
+    this.modClicked.emit(this.mod);
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
