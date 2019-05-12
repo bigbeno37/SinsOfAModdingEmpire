dad8ca4140f29d42e63b9c3aff36279a8dd59510
bigbeno37 <bigbeno37@gmail.com>
2019-05-12 12:02:08 +1000

    On load, SoaME will show the last played mod (if none was found, it will default to normal sins)

--- src/react/State.ts
+++ src/react/State.ts
@@ -1,13 +1,14 @@
 import {observable} from 'mobx';
 import {Mod} from '../shared/models/Mod';
 import EnabledModsBuilder from '../shared/EnabledModsBuilder';
+import {DB} from '../electron/DB';
 
 class State {
     @observable mods: Mod[] = [
         new Mod('Sins of a Solar Empire: Rebellion', 'Ironclad Studios', 'The vanilla experience', new EnabledModsBuilder().toString()),
         new Mod('Star Trek: Armada III', 'Somebody', 'A star trek mod', new EnabledModsBuilder().addMod('STA3_UPRISING').toString())
     ];
-    @observable selectedMod: Mod = this.mods[0];
+    @observable selectedMod: Mod = this.mods.find(mod => mod.name === new DB().lastPlayedMod) || this.mods[0];
 }
 
 export let state = new State();
